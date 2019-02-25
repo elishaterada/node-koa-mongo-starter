8b79bcaf654801a1924c9c8b0d8e1446c496e973
Elisha Terada <elishaterada@gmail.com>
2019-02-25 01:00:12 -0800

    Support deleting user

--- src/resources/user/user.controllers.js
+++ src/resources/user/user.controllers.js
@@ -15,6 +15,27 @@ const getMe = async ctx => {
       id: user._id,
       email: user.email
     };
+    ctx.status = 201;
+  } catch (e) {
+    console.error(e);
+    ctx.status = 404;
+  }
+};
+
+const deleteMe = async ctx => {
+  if (!ctx.state.user) {
+    ctx.status = 400;
+    return;
+  }
+
+  try {
+    await User.findByIdAndDelete(ctx.state.user.id)
+      .lean()
+      .exec();
+
+    ctx.body = {
+      message: "Successfully deleted your account"
+    };
     ctx.status = 200;
   } catch (e) {
     console.error(e);
@@ -22,4 +43,4 @@ const getMe = async ctx => {
   }
 };
 
-export { getMe };
+export { getMe, deleteMe };
--- src/resources/user/user.router.js
+++ src/resources/user/user.router.js
@@ -1,10 +1,15 @@
 import Router from "koa-router";
 import passport from "koa-passport";
 
-import { getMe } from "./user.controllers";
+import { deleteMe, getMe } from "./user.controllers";
 
 const router = new Router();
 
 router.get("/me", passport.authenticate("jwt", { session: false }), getMe);
+router.delete(
+  "/me",
+  passport.authenticate("jwt", { session: false }),
+  deleteMe
+);
 
 export default router.routes();
